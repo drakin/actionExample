552fc58fd9b54ceb5901289a04d486bf6cfe2735
Kobyshev Sergey <sergey.kobyshev@smddev.com>
2017-08-24 17:15:27 +0300

    add async to controller

--- src/main/java/org/kobyshev/controller/ActionController.java
+++ src/main/java/org/kobyshev/controller/ActionController.java
@@ -3,6 +3,7 @@ package org.kobyshev.controller;
 import org.kobyshev.model.Action;
 import org.kobyshev.service.ActionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,43 +11,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Controller
 public class ActionController {
     private ActionService actionService;
 
     @Autowired
-    public ActionController(ActionService actionService){
+    public ActionController(ActionService actionService) {
         this.actionService = actionService;
     }
 
+    @Async
     @RequestMapping(value = "/action/add", method = RequestMethod.POST)
     @ResponseBody
-    public Action addAction(@RequestBody Action action) {
-        return this.actionService.addAction(action);
+    public CompletableFuture<Action> addAction(@RequestBody final Action action) {
+        return CompletableFuture.supplyAsync(() -> actionService.addAction(action));
     }
 
+    @Async
     @RequestMapping(value = "/action/lastSecond")
     @ResponseBody
-    public List<Action> getLastSecondActions() {
-        return this.actionService.getLastSecondActions();
+    public CompletableFuture<List<Action>> getLastSecondActions() {
+        return CompletableFuture.supplyAsync(() -> this.actionService.getLastSecondActions());
     }
 
+    @Async
     @RequestMapping(value = "/action/lastMinute")
     @ResponseBody
-    public List<Action> getLastMinuteActions() {
-        return this.actionService.getLastMinuteActions();
+    public CompletableFuture<List<Action>> getLastMinuteActions() {
+        return CompletableFuture.supplyAsync(() -> this.actionService.getLastMinuteActions());
     }
 
+    @Async
     @RequestMapping(value = "/action/lastHour")
     @ResponseBody
-    public List<Action> getLastHourActions() {
-        return this.actionService.getLastHourActions();
+    public CompletableFuture<List<Action>> getLastHourActions() {
+        return CompletableFuture.supplyAsync(() -> this.actionService.getLastHourActions());
     }
 
+    @Async
     @RequestMapping(value = "/action/lastDay")
     @ResponseBody
-    public List<Action> getLastDayActions() {
-        return this.actionService.getLastDayActions();
+    public CompletableFuture<List<Action>> getLastDayActions() {
+        return CompletableFuture.supplyAsync(() -> this.actionService.getLastDayActions());
     }
 }
